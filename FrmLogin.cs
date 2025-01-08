14ce5600c7d556149d570e9777d0b795b34fceca
Ivado1 <ivadogames@mail.ru>
2025-01-08 14:40:07 +0800

    Create FrmLogin.cs

---
+++ FrmLogin.cs
@@ -0,0 +1,88 @@
+﻿using Npgsql;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Drawing;
+using System.Threading.Tasks;
+using System.Linq;
+using System.Text;
+using System.Data;
+using System.Windows;
+using System.Configuration;
+using System.Windows.Forms;
+using System.Diagnostics;
+
+namespace Library_App
+{
+    public partial class FrmLogin : Form
+    {
+        string strConnection = "Server=; port=; user id=; password=; database=;"; //put yours values (method not safe use only for test)
+
+        NpgsqlConnection connectNpg = new NpgsqlConnection();
+        NpgsqlDataReader reader;
+
+        public FrmLogin()
+        {
+            InitializeComponent();
+        }
+        private void closeConnect()
+        {
+            if (connectNpg.State == ConnectionState.Open) connectNpg.Close();
+        }
+        private void openConnect()
+        {
+            if (connectNpg.State == ConnectionState.Closed) connectNpg.Open();
+        }
+        private void closeButton_Click(object sender, EventArgs e)
+        {
+            //sql.connectionLeave();
+            this.Close();
+        }
+
+        private void FrmLogin_Load(object sender, EventArgs e)
+        {
+            connectNpg.ConnectionString = strConnection;
+            
+            loginField.Text = string.Empty;
+            passField.Text = string.Empty;
+
+            //loginField.Text = "ADMIN";  //For test
+            //passField.Text = "123";   //For test
+        }
+
+        private void buttonLogin_Click(object sender, EventArgs e)
+        {
+            openConnect();
+            if (loginField.Text.Trim() != string.Empty || passField.Text.Trim() != string.Empty)
+            {
+                NpgsqlCommand comm = new NpgsqlCommand("SELECT * FROM users", connectNpg);
+                reader = comm.ExecuteReader();
+                
+                while (reader.Read())
+                {
+                    if (loginField.Text == reader["login"].ToString() && passField.Text==reader["pass"].ToString())
+                    {
+                        if (reader["admin"].ToString()=="True")
+                        {
+                            MessageBox.Show("Admin Enter");
+                            this.Hide();
+                            FrmShowData newWindow = new FrmShowData();
+                            newWindow.ShowDialog();
+                            this.Close();
+                        }
+                        else
+                        {
+                            MessageBox.Show("User Enter");
+                            this.Hide();
+                            FrmTable newWindow = new FrmTable();
+                            newWindow.ShowDialog();
+                            this.Close();
+                        }
+                    }
+                }
+                if (comm != null) comm.Dispose();
+            }
+            closeConnect();
+        }
+    }
+}
